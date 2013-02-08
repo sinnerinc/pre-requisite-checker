ad86a2a377b52c7a56c5afcea5511d467de768eb
mknyc <mknyc@future-processing.com>
2013-02-08 16:52:24 +0100

    Checking for sql server version

---
+++ Prerequisites.Validator/Products/SqlServer/SqlServerValidator.cs
@@ -0,0 +1,46 @@
+﻿namespace Prerequisites.Validator.Products.SqlServer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.Win32;
+
+    public class SqlServerValidator
+    {
+        public bool SqlServerVersionExists(string version)
+        {
+            RegistryKey baseKey = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey rk = baseKey.OpenSubKey(@"SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL");
+            if (rk == null)
+            {
+                return false;
+            }
+
+            string[] instances = rk.GetValueNames();
+            if (instances.Length == 0)
+            {
+                return false;
+            }
+
+            foreach (var instance in instances)
+            {
+                string instanceSubkey = string.Concat(@"SOFTWARE\Microsoft\Microsoft SQL Server\", instance, @"\MSSQLServer\CurrentVersion");
+                RegistryKey instanceRegKey = Registry.LocalMachine.OpenSubKey(instanceSubkey);
+                if (instanceRegKey == null)
+                {
+                    continue;
+                }
+
+                string instanceVersion =  (string)instanceRegKey.GetValue("CurrentVersion");
+                if (version == instanceVersion)
+                {
+                    return true;
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
---
+++ Prerequisites.Validator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using Prerequisites.Validator.Products.SqlServer;
+
+namespace Prerequisites.Validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FindSqlServerR2();
+
+            
+        }
+
+        private static void FindSqlServerR2()
+        {
+            var sqlServerValidator = new SqlServerValidator();
+
+            string versionToFind = "10.50.2500.0";
+            Console.WriteLine("Looking for sql version: " + versionToFind);
+
+            bool r2sqlExists = sqlServerValidator.SqlServerVersionExists(versionToFind);
+            if (r2sqlExists)
+                Console.WriteLine("SQL Server version exists");
+            else
+                Console.WriteLine("SQL Server version does not exist");
+        }
+    }
+}
